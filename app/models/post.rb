9f7ed7cbcbecaf3544242289397867f1c8b8a810
Fallibi <119268948+Fallibi@users.noreply.github.com>
2025-08-11 20:57:20 -0700

    added validatios to user model. Created the Post model and added validations to it. Created the comment model and added validations to it.Passed all tests in Rails Console

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :user_id, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, 
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
